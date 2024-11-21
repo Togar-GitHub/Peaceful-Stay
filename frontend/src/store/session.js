e2969a194ff573bc548dc31dd58661b24c8dbc76
Togar-GitHub <togar_unix@Togar-Laptop>
2024-11-20 21:43:30 -0800

    front end authenticate phase 2 is done

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -6,58 +6,75 @@ const REMOVE_USER = 'session/REMOVE_USER';
 
 // ACTION CREATORS
 const setUser = (user) => {
-    return {
-        type: SET_USER,
-        payload: user
-    }
+  return {
+    type: SET_USER,
+    payload: user
+  }
 }
 
 const removeUser = () => {
-    return {
-        type: REMOVE_USER
-    }
+  return {
+    type: REMOVE_USER
+  }
 }
 
 // THUNK 
 export const login = (user) => async (dispatch) => {
-    const { credential, password } = user
-    const res = await csrfFetch('/api/session', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            credential,
-            password
-        })
+  const { credential, password } = user
+  const res = await csrfFetch('/api/session', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      credential,
+      password
     })
+  })
 
-    if (res.ok) {
-        const data = await res.json();
-        dispatch(setUser(data.user));
-        return res;
-    }
+  if (res.ok) {
+    const data = await res.json();
+    dispatch(setUser(data.user));
+    return res;
+  }
 }
 
 export const restoreUser = () => async (dispatch) => {
-    const response = await csrfFetch('/api/session');
-    const data = await response.json();
-    dispatch(setUser(data.user));
-    return response;
+  const response = await csrfFetch('/api/session');
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
 };
+
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch('/api/users', {
+    method: 'POST',
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password
+    })
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+}
 
 // REDUCERS
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
-    switch(action.type) {
-        case SET_USER:
-            return { ...state, user: action.payload }
+  switch(action.type) {
+    case SET_USER:
+      return { ...state, user: action.payload }
 
-        case REMOVE_USER:
-            return { ...state, user: null}
+    case REMOVE_USER:
+      return { ...state, user: null}
 
-        default:
-            return state
-    }
+    default:
+      return state
+  }
 }
 
 export default sessionReducer;
